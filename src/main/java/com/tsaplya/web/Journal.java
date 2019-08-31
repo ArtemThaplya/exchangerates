aa1562dea4ca2919c76da19d8f6e7ef5e80651dd
ArtemThaplya <artem.thaplya@gmail.com>
2019-08-31 23:54:36 +0300

    add Journal(pojo)

---
+++ src/main/java/com/tsaplya/web/Journal.java
@@ -0,0 +1,63 @@
+package com.tsaplya.web;
+
+import java.math.BigDecimal;
+
+
+public class Journal {
+    private int id;
+    private String datetime;
+    private String date;
+    private int currencyCode;
+    private BigDecimal rateBuy;
+    private BigDecimal rateSell;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getCurrencyCode() {
+        return currencyCode;
+    }
+
+    public void setCurrencyCode(int currencyCode) {
+        this.currencyCode = currencyCode;
+    }
+
+    public BigDecimal getRateBuy() {
+        return rateBuy;
+    }
+
+    public void setRateBuy(BigDecimal rateBuy) {
+        this.rateBuy = rateBuy;
+    }
+
+    public BigDecimal getRateSell() {
+        return rateSell;
+    }
+
+    public void setRateSell(BigDecimal rateSell) {
+        this.rateSell = rateSell;
+    }
+
+    @Override
+    public String toString() {
+        return "Journal{" +
+                "date='" + date + '\'' +
+                ", currencyCode=" + currencyCode +
+                ", rateBuy=" + rateBuy +
+                ", rateSell=" + rateSell +
+                '}';
+    }
+}
